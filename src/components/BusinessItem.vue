8fa2738c2bfef31286bf420aaa0e60fef8417b03
anas amir <ganmaazuma02@gmail.com>
2019-09-11 10:44:23 +0800

    Setting up Vue boilerplate
    Installed Vue and the main dependencies; Added BusinessItems and BusinessItem components;

---
+++ src/components/BusinessItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-col xl="3" lg="4" md="6" sm="12">
+    <b-card
+      title="Business Name"
+      img-src="https://picsum.photos/600/300/?image=25"
+      img-alt="Image"
+      img-top
+      tag="article"
+      class="mb-4"
+    >
+      <b-card-text>This will be the business short summary.</b-card-text>
+
+      <b-button href="#" variant="primary">Go to the details</b-button>
+    </b-card>
+  </b-col>
+</template>
+
+<script>
+export default {
+  name: "BusinessItem"
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/BusinessItems.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-jumbotron header="Business Hub" lead="A place where every business is connected"></b-jumbotron>
+    <b-container>
+      <b-row>
+        <BusinessItem /> <!-- Going to add v-for here -->
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import BusinessItem from "./BusinessItem";
+export default {
+  name: "BusinessItems",
+  components: {
+    BusinessItem
+  }
+};
+</script>
+
+<style>
+</style>
